3ce444dcc9e0cdc438e4a2aacd59731c3473e8e1
Helia Valiani <valiani@sp.local>
2024-12-31 15:55:22 +0330

    update version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='spay',
-    version='1.0.0',
+    version='1.0.1',
     author='Saba Pardazesh',
     author_email='info@sabapardazesh.net',
     description='Python SDK for Spay internet gateway',
